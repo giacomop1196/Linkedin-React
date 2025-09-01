5896d58e8927ed0f187e5fd79fbfca59623f0256
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-09-01 15:55:49 +0200

    commit sidebar

--- linkedin-react/src/App.jsx
+++ linkedin-react/src/App.jsx
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Importazione delle icone Bootstrap
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 // Importazione del componente RightSidebar
-import RightSidebar from './components/RightSidebar';
 import NavBar from './assets/components/Navbar';
 import LinkedinProfileSection from './assets/components/LinkedinProfileSection';
 import Footer from './assets/components/Footer';
@@ -16,8 +15,6 @@ import Footer from './assets/components/Footer';
 function App() {
   return (
     <>
-      {/* Rendering del componente RightSidebar che mostra la sidebar destra in stile LinkedIn */}
-      <RightSidebar />
      <NavBar/>
      <LinkedinProfileSection/>
      <Footer/>
