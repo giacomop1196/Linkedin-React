fa2be19f5a6ffa8b99972087385bcf77ebfbde7d
alessandraciccone <alessandraciccone@icloud.com>
2025-09-01 11:06:01 +0200

    first commit

--- linkedin-react/src/App.jsx
+++ linkedin-react/src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import Footer from "Components/Footer";
 
 function App() {
-
   return (
     <>
-     
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
