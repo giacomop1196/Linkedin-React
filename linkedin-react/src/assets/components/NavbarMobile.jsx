409a819a13b0b767d3c69c32b15670bd70064437
GerardoCasciano <dinocasciano203@gmail.com>
2025-09-04 14:58:24 +0200

    NavBar2

--- linkedin-react/src/assets/components/NavbarMobile.jsx
+++ linkedin-react/src/assets/components/NavbarMobile.jsx
@@ -1,6 +1,6 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 import { Container, Row, Col, Image } from "react-bootstrap";
 import "../css/MediaQueries.css";
 
